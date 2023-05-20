88dd3758bfcd5b9ee93d97e7f571a2fce7937165
DevikaSomagani <devikasomagani@gmail.com>
2023-05-20 12:46:50 +0530

    Student first version

---
+++ src/components/edit-student.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default class EditStudent extends React.Component{
+    render(){
+        return(
+            <>
+            <h1>EditStudent</h1>
+            </>
+        )
+    }
+}
---
+++ src/components/student-info.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default class StudentInfo extends React.Component{
+    render(){
+        return(
+            <>
+            <h1>StudentInfo</h1>
+            </>
+        )
+    }
+}
